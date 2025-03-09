9606382ae0e37b472b98ce24a69ddcf12fe678ec
talib <talibkhan172001.com>
2025-03-10 01:11:32 +0530

    Updates

---
+++ precache-manifest.7282df42d342022bb54961f3b264e1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80137da46cf0900f78c134023f12d66",
+    "url": "/Travel-react-app/index.html"
+  },
+  {
+    "revision": "be09a5f4ede2a69df839",
+    "url": "/Travel-react-app/static/css/main.a24bd626.chunk.css"
+  },
+  {
+    "revision": "da4d9dcc99581e96b65d",
+    "url": "/Travel-react-app/static/js/2.0a737b0f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Travel-react-app/static/js/2.0a737b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be09a5f4ede2a69df839",
+    "url": "/Travel-react-app/static/js/main.e2e2e334.chunk.js"
+  },
+  {
+    "revision": "a771cd4fed5c21f5825d",
+    "url": "/Travel-react-app/static/js/runtime-main.479297e3.js"
+  }
+]);
